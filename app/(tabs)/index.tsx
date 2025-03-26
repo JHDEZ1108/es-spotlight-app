4f953e79943f62198047d10f5426382ce5d44e4f
Josué Hernández <joheca1998@outlook.com>
2025-03-26 14:26:21 -0600

    Add Post component with image, actions, and dynamic links
    - Setup Post layout with interactive icons and link to notifications.
    - Manage image display and caching with expo-image.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import { ScrollView, Text, View } from 'react-native';
+
 import { useTheme } from '@/context/ThemeProvider'; 
 import { createStyles } from '@/styles/feed.styles';
 import ThemeToggle from '@/components/ThemeToggle';
+import { ThemeColors } from 'ThemeTypes';
+
 import { STORIES } from '@/constants/mock-data';
+import { useQuery } from 'convex/react';
+import { api } from '@/convex/_generated/api';
+
 import Story from '@/components/Story';
+import { Loader } from '@/components/Loader';
+import Post from '@/components/Post';
 
 export default function Index() {
   const { theme } = useTheme();
   const styles = createStyles(theme);
+  
+  const posts = useQuery(api.posts.getFeedPost);
+  if(posts === undefined) return <Loader />
+  if(posts.length === 0) return <NoPostsFound theme={theme}/>
   
   return (
     <View style={styles.container}>
@@ -19,7 +31,10 @@ export default function Index() {
       </View>
       
       {/* STORY COMPONENT */}
-      <ScrollView showsVerticalScrollIndicator={false}>
+      <ScrollView 
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 80 }}
+      >
         <ScrollView
           horizontal
           showsHorizontalScrollIndicator={false}
@@ -29,8 +44,24 @@ export default function Index() {
             <Story key={story.id} story={story}/>
           ))}
         </ScrollView>
+        {posts.map((post => (
+          <Post key={post._id} post={post} />
+        )))}
       </ScrollView>
       
     </View>
   )
 }
+
+const NoPostsFound = ({ theme }: { theme: ThemeColors }) => (
+  <View
+    style={{
+      flex: 1,
+      backgroundColor: theme.background,
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <Text style={{ fontSize: 20, color: theme.primary }}>No posts yet</Text>
+  </View>
+);
