df69d3b16cddee2dcc7794b956ee2c6691fc8851
Josué Hernández <joheca1998@outlook.com>
2025-03-26 22:12:45 -0600

    feat: add getUserByClerkId query to fetch user by Clerk ID

--- convex/users.ts
+++ convex/users.ts
@@ -1,4 +1,4 @@
-import { mutation, MutationCtx, QueryCtx } from "./_generated/server";
+import { mutation, MutationCtx, query, QueryCtx } from "./_generated/server";
 import { v } from "convex/values";
 
 export const createUser = mutation({
@@ -48,4 +48,18 @@ export async function getAuthenticatedUser(ctx: QueryCtx | MutationCtx) {
   if(!currentUser) throw new Error("User not found");
   
   return currentUser;
-}
+};
+
+export const getUserByClerkId = query({
+  args: { clerkId: v.string() },
+
+  handler: async (ctx, args) => {
+    // Query the users table using the "by_clerk_id" index
+    const user = await ctx.db
+      .query("users")
+      .withIndex("by_clerk_id", (q) => q.eq("clerkId", args.clerkId))
+      .unique(); // Ensure only one user is returned
+
+    return user;
+  },
+});
